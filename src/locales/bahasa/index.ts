0ff708b1873a13f6586f6c37c51be26a7373af57
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-06-13 16:16:56 +0700

    Update index.ts

--- src/locales/bahasa/index.ts
+++ src/locales/bahasa/index.ts
@@ -7,7 +7,7 @@ import Bahasa_questions_Locale from './questions.json';
 import Bahasa_axises_locale from './axis.json';
 import Bahasa_mathes_locale from './matches.json';
 
-export default const id = {
+export default {
   index: Bahasa_Locale,
   quiz: Bahasa_QUIZ_Locale,
   questions: Bahasa_questions_Locale,
