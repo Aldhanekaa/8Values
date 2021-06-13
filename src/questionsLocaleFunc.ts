fc192427a803277c41e106f9b93a6b823f37347b
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-06-13 11:50:49 +0700

    Update questionsLocaleFunc.ts

--- src/questionsLocaleFunc.ts
+++ src/questionsLocaleFunc.ts
@@ -1,9 +1,4 @@
-import { LanguagesT, Languages } from './redux/actions/lang';
-import { useEffect } from 'react';
 import i18next from 'i18next';
-
-import Bahasa_questions_Locale from './locales/bahasa/questions.json';
-import EN_questions_Locale from './locales/en/questions.json';
 
 export default function GetQuestionBasedOnLanguage() {
   // @ts-ignore
