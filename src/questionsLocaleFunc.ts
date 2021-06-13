4ad62aaf95b8cc615f6358b0068ce86ab9cd3c20
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-06-13 12:20:27 +0700

    Fix line 1 error
    fix error at line 1 of file querstionsLocaleFunc.ts

--- src/questionsLocaleFunc.ts
+++ src/questionsLocaleFunc.ts
@@ -1,8 +1,5 @@
-import { LanguagesT, Languages } from './redux/actions/lang';
-import { useEffect } from 'react';
 import i18next from 'i18next';
 
-import Bahasa_questions_Locale from './locales/bahasa/questions.json';
 import EN_questions_Locale from './locales/en/questions.json';
 
 export default function GetQuestionBasedOnLanguage() {
